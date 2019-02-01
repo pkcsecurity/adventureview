b3aca606d65be7e9c742d10e6b1261581300d7d1
a5834929 <a5834929@gmail.com>
2019-02-01 15:47:41 -0800

    remove users route

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/', indexRouter);
 app.use('/game', indexRouter);
-app.use('/users', usersRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
