8b08104d9605fb817b02cd90a043df0cba9e5dda
a5834929 <a5834929@gmail.com>
2019-02-01 13:51:03 -0800

    several stylesheet changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PKC Hiring'});
+  res.render('index', { title: 'PKC Hiring'})
 });
 
 module.exports = router;
