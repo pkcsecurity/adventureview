05be9f18e8a5c433b8bfcf7941fbe2c80fc84af9
a5834929 <a5834929@gmail.com>
2019-02-01 14:33:48 -0800

    add route to the starting page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PKC Hiring'})
+  res.render('index', { title: 'PKC Hiring'});
+});
+
+router.get('/start', function(req, res, next){
+  res.render('start', { title: 'Corporate Peon'});
 });
 
 module.exports = router;
